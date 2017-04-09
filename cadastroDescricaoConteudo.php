3f0bbd4d1d1540d58ee04776caaedd34794498a6
AlexBarazal <alexbarazal@hotmail.com>
2017-04-09 18:42:45 -0300

    Telas Pasta arquivo, Conteúdo, Descrição Conteúdo

--- cadastroDescricaoConteudo.php
+++ cadastroDescricaoConteudo.php
@@ -17,56 +17,43 @@
         <section id="contact">
             <div class="container">
                 <br>
-                <h2 class="text-center">Cadastro da Pasta de Arquivos</h2>
+                <h2 class="text-center">Cadastro do Conteúdo</h2>
                 <hr>
                 <div class="row">
                     <div class="col-lg-8 offset-lg-2">
                         <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
                         <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                        <form name="cadastroPastaArquivo" id="pastaArquivoForm" novalidate>
+                        <form name="cadastroDescricaoConteudo" id="descricaoConteudoForm" novalidate>
                              <div class="row control-group">
                                 <div class="form-group col-xs-12 floating-label-form-group controls">
-                                    <label for="nomeProdutor">Nome Cliente</label>
-                                    <!-- Este campo vai pegar o nome do cliente logado-->
-                                    <input type="text" name="nomeCliente" class="form-control" placeholder="Nome Cliente" id="nomeClientePastaArquivo" maxlength="100"  required data-validation-required-message="Por Favor insira seu nome">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="row control-group">
-                                <div class="form-group col-xs-12 floating-label-form-group controls">
                                     <label for="nomePasta">Nome da Pasta</label>
-                                    <input type="text" name="nomePasta" class="form-control" placeholder="Nome da Pasta" id="nomePastaArquivo" maxlength="80"  required data-validation-required-message="Por Favor insira seu nome">
-                                    <p class="help-block text-danger"></p>
+                                    <!-- Este campo vai pegar o nome da pasta deste conteúdo-->
+                                    <input type="text" name="nomePasta" class="form-control" placeholder="Nome da Pasta" id="nomePastaArquivo" maxlength="80">
                                 </div>
                             </div>
                             <div class="row control-group">
                                 <div class="form-group col-xs-12 floating-label-form-group controls">
-                                    <label for="url">Link da Pasta</label>
-                                    <input type="text" name="url" class="form-control" placeholder="Link da Pasta" id="urlPastaArquivo" maxlength="200"  required data-validation-required-message="Por Favor insira seu nome">
-                                    <p class="help-block text-danger"></p>
+                                    <label for="nomeConteudo">Nome Conteúdo</label>
+                                    <!-- Este campo vai pegar o nome da pasta deste conteúdo-->
+                                    <input type="text" name="nomeConteudo" class="form-control" placeholder="Nome Conteúdo" id="nomeConteudo" maxlength="100">
                                 </div>
                             </div>
                             <div class="row control-group">
                                 <div class="form-group col-xs-12 floating-label-form-group controls">
-                                    <label for="tamanho">Tamanho do Arquivo</label>
-                                    <input type="number" name="tamanho" class="form-control" placeholder="Tamanho" id="tamanhoPastaArquivo">
+                                    <label for="conteudoProgramatico">Conteúdo Programático</label>
+                                    <input type="text" name="conteudoProgramatico" class="form-control" placeholder="Conteúdo Programático" id="conteudoProgramatico" maxlength="100">
                                 </div>
                             </div>
                             <div class="row control-group">
                                 <div class="form-group col-xs-12 floating-label-form-group controls">
-                                    <label for="formato">Formato do Arquivo(ex: .mp4, .pdf)</label>
-                                    <input type="text" name="formato" class="form-control" placeholder="Formato do Arquivo" id="formatoPastaArquivo">
+                                    <label for="objetivo">Objetivo</label>
+                                    <input type="text" name="objetivo" class="form-control" placeholder="Objetivo Programático" id="objetivo" maxlength="100">
                                 </div>
                             </div>
                             <div class="row control-group">
                                 <div class="form-group col-xs-12 floating-label-form-group controls">
-                                    <label for="conteudo">Conteudo</label>
-                                     <div id="success"></div>
-                                    <div class="row">
-                                        <div class="form-group col-xs-12">
-                                            <button type="submit" class="btn btn-success btn-lg">Adicionar Conteúdo</button>
-                                        </div>
-                                    </div>
+                                    <label for="quantidadeArquivos">quantidadeArquivos</label>
+                                    <input type="text" name="quantidadeArquivos" class="form-control" placeholder="Quantidade de Arquivos" id="quantidadeArquivos" maxlength="100">
                                 </div>
                             </div>
                             <br>
