c59082a23623bffadf73d2325a3103e6120f99fb
renanrosas <renan-rosas@hotmail.com>
2017-05-03 21:49:15 -0300

    menu

--- cabecalho.php
+++ cabecalho.php
@@ -1,4 +1,7 @@
-    <?php session_start ();?>
+    <?php session_start ();
+        $nivel_acesso = $_SESSION['userNivel'];
+    
+    ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
